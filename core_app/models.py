c795106e4c64e6294485447286b25ff8c4a8c44a
Nixo <diego.llorens@virginiogomez.cl>
2026-05-10 16:49:09 -0400

    agregar libros completamente funcional y Libreria Pillow instalada para la manipulacion de imagenes

--- core_app/models.py
+++ core_app/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Libro(models.Model):
+    nombre = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to='portadas/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
--- core_app/views.py
+++ core_app/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required   
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .models import Libro
 
 def home(request):
     return render(request, 'core/home.html')
 
 def catalog(request):
-    return render(request, "core/catalog.html")
+    libros_db = Libro.objects.all()
+    return render(request, "core/catalog.html", {"libros": libros_db})
 
 def register(request):
     if request.method == 'POST':
@@ -20,10 +22,22 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
-# Solo usuarios logueados pueden entrar aquí
+
 @login_required(login_url='login')
 def agregar_libro(request):
     if request.method == 'POST':
-         
+
+        nombre_libro = request.POST.get('nombre')
+        precio_libro = request.POST.get('precio')
+        imagen_libro = request.FILES.get('imagen')
+
+
+        nuevo_libro = Libro(
+            nombre=nombre_libro,
+            precio=precio_libro,
+            imagen=imagen_libro
+        )
+        nuevo_libro.save()
+
         return redirect('catalog')
     return render(request, 'core/agregar.html')
