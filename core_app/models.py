3dd3c96b0e800f666a902e310c1a814668f28414
maxssj <figueroamaximo7@gmail.com>
2026-05-12 16:41:17 -0400

    actualizacion total de los estilos de botones y fondo agrege una ventana emergente en el catalogo para mas info de los libros

--- core_app/models.py
+++ core_app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-
 class Libro(models.Model):
     nombre = models.CharField(max_length=200)
+    autor = models.CharField(max_length=200, default="Anónimo") 
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     imagen = models.ImageField(upload_to='portadas/', null=True, blank=True)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} - {self.autor}"
